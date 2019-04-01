1470cce3a83c901f1a07e4e12d0311c212f0aba9
rasurathore13 <30598607+rasurathore13@users.noreply.github.com>
2019-04-01 18:09:15 +0530

    CSS Loader
    Added files for CSS Loader In Login Component, Home Component and Signup Component.

--- PhoneReviewApp/src/app/LogInComponent/login.component.ts
+++ PhoneReviewApp/src/app/LogInComponent/login.component.ts
@@ -14,29 +14,37 @@ export class LoginComponent {
   email: string;
   password: string;
   isAUserLoggedIn: boolean;
+  isSubmitButtonClicked = false;
 
   constructor(private http: HttpClient, private loginService: LoginService, private router: Router) {
     if (localStorage.getItem('JWTAccessToken')) {
       this.isAUserLoggedIn = true;
+      this.router.navigate(['/Home']);
     } else {
       this.isAUserLoggedIn = false;
     }
   }
 
   logIn(loginForm: NgForm) {
-    if(loginForm.value.email.length === 0 || loginForm.value.password.length === 0) {
-      alert('Both the fields are mandatory');
+    this.isSubmitButtonClicked = true;
+    if (loginForm.valid) {
+      if(loginForm.value.email.length === 0 || loginForm.value.password.length === 0) {
+        alert('Both the fields are mandatory');
+        this.isSubmitButtonClicked = false;
+      } else {
+        this.loginService.loginUser(loginForm)
+        .subscribe(response => {
+          let JWTAccessToken = JSON.stringify(response);
+          localStorage.setItem('JWTAccessToken', JWTAccessToken.substring(1, JWTAccessToken.length - 1));
+          window.location.reload();
+        },
+        error => {
+          alert(JSON.stringify(error.error));
+          this.isSubmitButtonClicked = false;
+        });
+      }
     } else {
-      this.loginService.loginUser(loginForm)
-      .subscribe(response => {
-        let JWTAccessToken = JSON.stringify(response);
-        localStorage.setItem('JWTAccessToken', JWTAccessToken.substring(1, JWTAccessToken.length - 1));
-        window.location.reload();
-        this.router.navigate(['/Home']);
-      },
-      error => {
-        alert(error.error);
-      });
+      this.isSubmitButtonClicked = false;
     }
   }
 }
--- PhoneReviewApp/src/app/SignUpComponent/signup.component.ts
+++ PhoneReviewApp/src/app/SignUpComponent/signup.component.ts
@@ -3,6 +3,7 @@ import { SignupService } from './signup.service';
 import { NgForm } from '@angular/forms';
 import { LoginService } from '../LogInComponent/login.service';
 import { Router } from '@angular/router';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -14,22 +15,27 @@ export class SignupComponent {
   password: string;
   confirmPassword: string;
   isAUserLoggedIn: Boolean;
+  isSubmitButtonClicked = false;
 
   constructor(private router: Router, private signupService: SignupService, private loginService: LoginService) {
     if (localStorage.getItem('JWTAccessToken')) {
       this.isAUserLoggedIn = true;
+      this.router.navigate(['/Home']);
     } else {
       this.isAUserLoggedIn = false;
     }
   }
 
   signUp(signupForm: NgForm) {
+    this.isSubmitButtonClicked = true;
     if(signupForm.valid) {
       if(signupForm.value.password !== signupForm.value.confirmPassword) {
         alert('The Passwords Does not match. Please Check and try again.');
+        this.isSubmitButtonClicked = false;
       } else {
         if(signupForm.value.password.length < 8) {
           alert('The password Needs to be atlest 8 charaters long.');
+          this.isSubmitButtonClicked = false;
         } else {
           const regexp = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
           if(regexp.test(signupForm.value.email)) {
@@ -42,22 +48,27 @@ export class SignupComponent {
                   this.signupService.createUser(signupForm, user_ID).subscribe(response3 => {
                     alert('You are now registered and are eligible to make reviews');
                     window.location.reload();
-                    this.router.navigate(['/Home']);
+                    this.isSubmitButtonClicked = false;
                   },
-                  error => alert(error.error));
+                  error => alert(JSON.stringify(error.error)));
+                  this.isSubmitButtonClicked = false;
                 },
                 error => alert('Possibly you were registered. Please try logging In separately'));
+                this.isSubmitButtonClicked = false;
             },
             error => {
               alert(JSON.stringify(error.error));
+              this.isSubmitButtonClicked = false;
             });
           } else {
             alert('You need to enter a valid Email Address');
+            this.isSubmitButtonClicked = false;
           }
         }
       }
     } else {
       alert('You need to enter all the feilds');
+      this.isSubmitButtonClicked = false;
     }
   }
 }
